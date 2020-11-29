d235a6396b9a98c2e9e22bd8ebc4530ef8ff033d
Clay Bennington <claymation296@gmail.com>
2020-11-29 13:39:08 -0700

    Added logic for picker action buttons so they open the appropriate overlay. Added disabled state styles and functionality to picker buttons. Updated deps.

--- picker/acs-picker-overlay.js
+++ picker/acs-picker-overlay.js
@@ -36,12 +36,13 @@
   **/
 
 
-import {AppElement, html} from '@longlost/app-element/app-element.js';
-import {hijackEvent}      from '@longlost/utils/utils.js';
-import htmlString         from './acs-picker-overlay.html';
+import {AppElement, html}  from '@longlost/app-element/app-element.js';
+import {hijackEvent, warn} from '@longlost/utils/utils.js';
+import htmlString          from './acs-picker-overlay.html';
 import '@longlost/app-overlays/app-header-overlay.js';
 import '../app-camera-icons.js';
 import './acs-picker-button.js';
+// `app-camera-system` lazy imported.
 
 
 class ACSPickerOverlay extends AppElement {
@@ -55,14 +56,40 @@ class ACSPickerOverlay extends AppElement {
   static get properties() {
     return {
 
+      albumName: {
+        type: String,
+        value: 'My Photos'
+      },
+
+      // This MUST be unique to avoid unreachable data.
+      albumType: {
+        type: String,
+        value: 'photos'
+      },
+
       darkMode: Boolean,
+
+      // Set which camera to initialize with.
+      //
+      // NOTE: Many devices, such as laptops/pc do not 
+      //       have an 'environment' facing camera.
+      defaultCamera: {
+        type: String,
+        value: 'user' // Or 'environment'. 
+      },
+
+      // Include AR filters, stickers, effects for the human face.
+      faceAr: Boolean,
 
       title: {
         type: String,
         value: 'Change this Photo'
       },
 
-      user: Object,
+      user: {
+        type: Object,
+        value: null // Needed for initial button disabled state.
+      },
 
       _opened: Boolean
 
@@ -83,17 +110,47 @@ class ACSPickerOverlay extends AppElement {
 
 
   __resetHandler(event) {
-    hijackEvent(event);
-
     this._opened = false;
   }
 
 
-  __btnClickedRippledHandler(event) {
-    hijackEvent(event);
+  async __btnClickedRippledHandler(event) {
+    try {
 
-    console.log('btn clicked rippled: ', event.detail);
+      hijackEvent(event);
 
+      await import(
+        /* webpackChunkName: 'app-camera-system' */ 
+        '../app-camera-system.js'
+      );
+
+      switch (event?.detail?.type) {
+
+        case 'camera':
+          await this.$.cameraSystem.openCamera();
+          break;
+
+        case 'picker':
+          await this.$.cameraSystem.openSelector();
+          break;
+
+        case 'sources':
+          await this.$.cameraSystem.openSources();
+          break;
+
+        case 'roll':
+          await this.$.cameraSystem.openCameraRoll();
+          break;
+
+        default:
+          throw new Error('Incorrect picker button type.');
+      }
+    }
+    catch (error) {
+      console.error(error);
+
+      warn('Could not open the camera system.');
+    }
   }
 
 
